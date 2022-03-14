97a59c6835e45b4f2bda553f2185d9c0ba60f13e
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-03-14 19:52:01 -0300

    Atributos do heroi

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace JogoRpgPoo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string nome = "Arus";
+            int level = 12;
+            string tipoHeroi = "Guerreiro";
+
+            string nome2 = "AWedge";
+            int level2 = 20;
+            string tipoHeroi2 = "Ninja";
+
+            Console.WriteLine(nome + " " + level + " " + tipoHeroi);
+            Console.WriteLine(nome2 + " " + level2 + " " + tipoHeroi2);
+        }
+    }
+
+}
